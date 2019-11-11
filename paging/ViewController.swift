ac8985f80729c4ae2a997914c67298ab53b52e18
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2019-11-11 10:11:07 +0700

    Setup main view

--- paging/ViewController.swift
+++ paging/ViewController.swift
@@ -2,10 +2,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pageView: UIView!
+    @IBOutlet weak var skipButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        skipButton.layer.cornerRadius = 8
 
     }
 
+    @IBAction func skipButtonTapped(_ sender: Any) {
+    }
 }
 
