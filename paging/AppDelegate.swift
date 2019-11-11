43d0c7c81bdd7342d47cbb3f5b82c8e1c6002589
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2019-11-11 09:57:10 +0700

    Remove init comment

--- paging/AppDelegate.swift
+++ paging/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  paging
-//
-//  Created by Wasith Theerapattrathamrong on 11/11/2562 BE.
-//  Copyright © 2562 Wasith Theerapattrathamrong. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
